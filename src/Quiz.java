0d685dbfd6e107de9c478149bc7fcb5b944bde56
Horst Häring <Horst.Haering@dpdhl.com>
2023-02-03 11:02:41 +0100

    fehler behoben

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,23 +1,29 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Quiz {
     public static void main(String[] args) {
-
-
         String[] fragen = {"wie viel ist 5+4?", "wie viel ist 3*5"};
-        int [] antworten = {9, 15};
+        int[] antworten = {9, 15};
 
         int count = 0;
-        while(count < fragen.length){
+        while (count < fragen.length) {
             System.out.println(fragen[count]);
 
             Scanner scan;
             scan = new Scanner(System.in);
-            int eingabeUser = scan.nextInt();
+
+            int eingabeUser;
+            try {
+                eingabeUser = scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Fehler bei der Eingabe.");
+                continue;
+            }
 
             if (eingabeUser == antworten[count]) {
                 System.out.println("richtig");
-                count ++;
+                count++;
             } else {
                 System.out.println("falsch");
             }
